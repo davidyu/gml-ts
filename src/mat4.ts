10ab15eaab438ab251300fd7a93bd116e2e9581b
David Yu <lewen.yu@gmail.com>
2015-12-03 19:31:19 -0800

    fix build

--- src/mat4.ts
+++ src/mat4.ts
@@ -264,7 +264,7 @@ module gml {
   export function makeLookAt( pos: Vec4, aim: Vec4 /* target */, up: Vec4, right: Vec4 ): Mat4 {
     let x = right.normalized;
     let y = up.normalized;
-    let z = aim.sub( pos ).normalized;
+    let z = aim.subtract( pos ).normalized;
 
     var lookAt = makeMat4FromRows( x, y, z, new Vec4( 0, 0, 0, 1 ) );
     lookAt.tx = pos.x;

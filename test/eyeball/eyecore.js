54f3d8e3d372a44c2d1dff6fd2c5902afae0217e
David Yu <lewen.yu@gmail.com>
2016-09-25 23:41:15 -0700

    refactor out core canvas code out of eyeball tests

---
+++ test/eyeball/eyecore.js
@@ -0,0 +1,35 @@
+// globals
+var canvas;
+var canvas_width;
+var canvas_height;
+var context;
+
+function backingScale(context) {
+  if ( 'devicePixelRatio' in window ) {
+    if ( window.devicePixelRatio > 1 ) {
+      return window.devicePixelRatio;
+    }
+  }
+  return 1;
+}
+
+function initCanvas( elem ) {
+  if ( elem ) canvas = elem;
+  else        canvas = document.getElementById( "viewport" );
+
+  context = canvas.getContext( '2d' );
+
+  var scaleFactor = backingScale( context );
+
+  canvas_width   = canvas.width;
+  canvas_height  = canvas.height;
+
+  if ( scaleFactor > 1 ) {
+    canvas.width  *= scaleFactor;
+    canvas.height *= scaleFactor;
+    canvas.style.width  = canvas_width + "px";
+    canvas.style.height = canvas_height + "px";
+    context = canvas.getContext( '2d' ); // context has been updated
+    context.scale( scaleFactor, scaleFactor );
+  }
+}

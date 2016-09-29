ac8d1075d4a331338d903fbaac6dc8d4d6a63e39
David Yu <lewen.yu@gmail.com>
2016-09-29 10:28:16 -0700

    fix runtime error (gml namespacing)

--- test/eyeball/3d/perspective.js
+++ test/eyeball/3d/perspective.js
@@ -1,21 +1,21 @@
 // cached so we don't recompute these matrices every frame
-var cached_v  = Mat4.identity();
-var cached_p  = Mat4.identity();
-var cached_vp = Mat4.identity; // view-perspective matrix
+var cached_v  = gml.Mat4.identity();
+var cached_p  = gml.Mat4.identity();
+var cached_vp = gml.Mat4.identity(); // view-perspective matrix
 
 function setPerspective( fov, aspectRatio, near, far ) {
-  cached_p = makePerspective( fov, aspectRatio, near, far );
-  Mat4.matmul( cached_v, cached_p, cached_vp );
+  cached_p = gml.makePerspective( fov, aspectRatio, near, far );
+  gml.Mat4.matmul( cached_v, cached_p, cached_vp );
 }
 
 function setCamera( pos, aim, up, right ) {
-  cached_v = makeLookAt( pos, aim, up, right );
-  Mat4.matmul( cached_v, cached_p, cached_vp );
+  cached_v = gml.makeLookAt( pos, aim, up, right );
+  gml.Mat4.matmul( cached_v, cached_p, cached_vp );
 }
 
 function mapToScreen( in_vertex, screenw, screenh, out_vertex ) {
   if ( out_vertex != null ) {
-    Mat4.transform( cached_vp, in_vertex, out_vertex );
+    gml.Mat4.transform( cached_vp, in_vertex, out_vertex );
     out_vertex.x *= screenw;
     out_vertex.y *= screenh;
     return;

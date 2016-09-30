5529a0763edc8d08f87d886c50a5bf57c4b0b99f
David Yu <lewen.yu@gmail.com>
2016-09-30 10:48:32 -0700

    color polygon grey

--- test/eyeball/eyecore.js
+++ test/eyeball/eyecore.js
@@ -9,6 +9,7 @@ var context;
 // color constants
 var LIGHT_GREEN = '#7f7';
 var LIGHT_RED   = '#f77';
+var LIGHT_GREY  = '#ccc';
 var BLUE        = '#0000ff';
 var RED         = '#ff0000';
 var GREEN       = '#00ff00';

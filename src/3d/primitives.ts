90ad4264db4b7e0230f5d40053fd5425fb63d5c4
David Yu <lewen.yu@gmail.com>
2016-10-17 08:43:33 -0700

    plane primitive; expose primitives in gml

--- src/3d/primitives.ts
+++ src/3d/primitives.ts
@@ -10,7 +10,7 @@ module gml {
 
     static At( r: Ray, t: number ): Vec3 {
       var out: Vec3;
-      Vec3.scale( r.direction, t, out );
+      Vec3.multiply( r.direction, t, out );
       Vec3.add( r.point, out, out );
       return out; 
     }
@@ -18,5 +18,23 @@ module gml {
 
   export class Polygon {
     points: Vec4[];
+
+    constructor( points: Vec4[] ) {
+      this.points = points;
+    }
+  }
+
+  export class Plane {
+    normal: Vec4;
+    d: number;
+
+    constructor( normal: Vec4, d: number ) {
+      this.normal = normal;
+      this.d = d;
+    }
+
+    static fromCoefficients( coeffs: Vec4 ): Plane {
+      return new Plane( new Vec4( coeffs.x, coeffs.y, coeffs.z, 1 ), coeffs.w );
+    }
   }
 }

3effbddc64aa616f7ba943e8d8058a73637d7859
David Yu <lewen.yu@gmail.com>
2016-10-18 11:17:10 -0700

    3d halfspace categorization

--- src/3d/collision.ts
+++ src/3d/collision.ts
@@ -1,5 +1,20 @@
 module gml {
+  export enum Halfspace {
+    POSITIVE,
+    NEGATIVE,
+    COINCIDENT, // point exists on plane itself, and does not line on either halfspaces
+  }
+
   export class Collision {
-    
+    static CategorizeHalfspace( point: Vec4, plane: Plane ): Halfspace {
+      let dp = plane.normal.dot( point );
+      if ( Math.abs( dp - plane.d ) < EPSILON ) {
+        return Halfspace.COINCIDENT;
+      } else if ( dp > plane.d ) {
+        return Halfspace.POSITIVE;
+      } else {
+        return Halfspace.NEGATIVE;
+      }
+    }
   }
 }

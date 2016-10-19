383b259486ac7eae167d1ed6f91f1c798f82640d
David Yu <lewen.yu@gmail.com>
2016-10-19 14:51:18 -0700

    fix 3d halfspace test

--- src/3d/collision.ts
+++ src/3d/collision.ts
@@ -12,11 +12,20 @@ module gml {
   }
 
   export class Collision {
+    /**
+     * Given a point, classifies on which side of a plane it lies.
+     */
     static CategorizeHalfspace( point: Vec4, plane: Plane ): Halfspace {
+      // we know that for the plane, N dot p0 + d = 0 where p0 is a point on the plane
+      // we know that the point p is on the positive side of the halfspace iff:
+      //      N dot (p - p0) > 0
+      //   => N dot p - N dot p0 > 0
+      //   => N dot p - (-d) > 0 (substituting N dot p0 = -d)
+      //   => N dot p > -d
       let dp = plane.normal.dot( point );
-      if ( Math.abs( dp - plane.d ) < EPSILON ) {
+      if ( Math.abs( dp + plane.d ) < EPSILON ) {
         return Halfspace.COINCIDENT;
-      } else if ( dp > plane.d ) {
+      } else if ( dp > -plane.d ) {
         return Halfspace.POSITIVE;
       } else {
         return Halfspace.NEGATIVE;
